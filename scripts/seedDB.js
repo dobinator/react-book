7a9ed15252022a5d7607ca3517015a15414f55fa
grey cashmere <dobsonemily@gmail.com>
2021-04-28 19:34:11 -0700

    adding different books

--- scripts/seedDB.js
+++ scripts/seedDB.js
@@ -2,11 +2,11 @@ const { books } = require("../models");
 
 const booksData = [
   {
-    isbn: 9780345339683,
-    title: "The Hobbit, Or There and Back Again",
-    description:' "In a hole in the ground, there lived a hobbit." So begins one of the most beloved and delightful tales in the English language. Set in the imaginary world of Middle-earth, at once a classic myth and a modern fairy tale, The Hobbit is one of literature',
-    cover_img_url: "http://books.google.com/books/content?id=hFfhrCWiLSMC&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api",
-    author: "John Ronald Reuel Tolkien",
+    isbn: 9780191605505,
+    title: "Little Women",
+    description: "Little Women has remained enduringly popular since its publication in 1868, becoming the inspiration for a whole genre of family stories. Set in a small New England community, it tells of the March family: Marmee looks after daughters in the absence of her husband, who is serving as an army chaplain in the Civil War, and Meg, Jo,Beth, and Amy experience domestic trials and triumphs as they attempt to supplement..",
+    cover_img_url: "http://books.google.com/books/content?id=v0iuYlsc8EIC&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api",
+    author: "Louisa May Alcott",
   },
   {
     isbn: 9781408809327,
@@ -17,18 +17,18 @@ const booksData = [
   },
 
   {
-    isbn: 9781571317322,
-    title: "The Seed Keeper",
-    desciption: "Rosalie Iron Wing has grown up in the woods with her father, Ray, a former science teacher who tells her stories of plants, of the stars, of the origins of the Dakhóta people. Until, one morning, Ray doesn’t return from checking his traps. ", 
-    cover_img_url:"http://books.google.com/books/content?id=XVElEAAAQBAJ&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api",
-    author: "Diane Wilson",
+    isbn: 9780307950192,
+    title: "Tales from Watership Down",
+    desciption:"The sequel to \"Watership Down\" chronicles the lives of the rabbits after their defeat of General Woundwort, from the exploits of El-ahrairah, the mythical rabbit hero, to the adventures of Hazen, Fiver, Bigwig, and their friends.",  
+    cover_img_url:"http://books.google.com/books/content?id=XaeFNAu1yvEC&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api",
+    author:"Richard Adams",
   },
   {
-    isbn: 9780812996715,
-    title: "Miss Benson's Beetle",
-    description: "It is 1950. London is still reeling from World War II, and Margery Benson, a schoolteacher and spinster, is trying to get through life, surviving on scraps. One day, she reaches her breaking point, abandoning her job and small existence to set out on an expedition to the other side of the world in search of her childhood obsession: an insect that may or may not exist—the golden beetle of New Caledonia.", 
-    cover_img_url: "http://books.google.com/books/content?id=qLrWDwAAQBAJ&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api",
-    author: "Rachel Joyce",
+    isbn:  9780747585664,
+    title: "Eat, Pray, Love",
+    description: "The Number One international bestseller, Eat, Pray Love is a journey around the world, a quest for spiritual enlightenment and a story for anyone who has battled with divorce, depression and heartbreak.",
+    cover_img_url: "http://books.google.com/books/content?id=QdCNeNx6LJUC&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api",
+    author:"Elizabeth Gilbert",
   },
   {
     isbn: 9780156028356,
@@ -38,10 +38,10 @@ const booksData = [
     author: "Alice Walker",
   },
   {
-    isbn: 9780316498029,
-    title:"The Law of Innocence",
-    deacription: "On the night he celebrates a big win, defense attorney Mickey Haller is pulled over by police, who find the body of a former client in the trunk of his Lincoln. Haller is immediately charged with murder but can’t post the exorbitant $5 million bail slapped on him by a vindictive judge.", 
-    cover_img_url:"http://books.google.com/books/content?id=bGXSDwAAQBAJ&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api",
+    isbn: 9780759525771,
+    title:"The Black Echo",
+    deacription: "An LAPD homicide detective must choose between justice and vengeance as he teams up with the FBI in this \"thrilling\" novel filled with mystery and adventure (New York Times Book Review). For maverick LAPD homicide detective Harry Bosch, the body in the drainpipe at Mulholland Dam is more than another anonymous statistic. This one is personal . . . because the murdered man was a fellow Vietnam \"tunnel rat\" who had fought side by side with him in a hellish underground war. Now Bosch is about to relive the horror of Nam.",
+    cover_img_url:"http://books.google.com/books/content?id=gClL18CTQWIC&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api",
     author: "Michael Connelly"
   },
 ];
