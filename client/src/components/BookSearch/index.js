5a5dbe09970d6325c785ac9743052353a7c9ed7a
grey cashmere <dobsonemily@gmail.com>
2021-05-01 18:25:13 -0700

    updating files, using the footer

---
+++ client/src/components/BookSearch/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function BookSearch() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default BookSearch
---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Footer() {
+    return (
+        <footer class="footer">
+        <div class="content has-text-centered">
+          <p> 
+            <strong>Google Book Search</strong> by Emily Dobson
+          </p>
+        </div>
+      </footer>
+    )
+}
+
+export default Footer; 
+
