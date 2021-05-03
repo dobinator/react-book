06fc1ed468f59f5de59b39c03646229bd773adef
grey cashmere <dobsonemily@gmail.com>
2021-05-03 08:54:15 -0700

    adding required files to index.js

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ mongoose.connect(
 
 // If no API routes are hit, send the React app 
 app.use(function(req, res){
-res.sendFile(path.join(__dirname, "/client/build/index.html"));
+res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
 //start the API server
