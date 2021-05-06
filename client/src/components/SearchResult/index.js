51360224926d51af36afad20691189b7fed9814b
grey cashmere <dobsonemily@gmail.com>
2021-05-05 20:08:02 -0700

    fixing layout bookcard

--- client/src/components/SearchResult/index.js
+++ client/src/components/SearchResult/index.js
@@ -1,43 +1,46 @@
-import React from 'react'
-
+import React from "react";
 
 function SearchResult() {
-    return (
-<section className="container-flex">
+  return (
+    <section className="container-flex">
       <div className="columns">
-        <div className= "column">
-            <h2>Search Results</h2>
-        <div className="card">
-          <div className="media-content">
-            <p className="title is-4">{props.title}</p>
-            <p className="subtitle is-6">Written by {props.author}</p>
-          </div>
+        <div className="column">
+          <h2 style={{ fontSize: "18px" }}>Search Results</h2>
+          <div className="card">
+            <div className="media-content">
+              <p className="title is-4">{props.title}</p>
+              <p className="subtitle is-6">Written by {props.author}</p>
+            </div>
 
-          <div className="card-image">
-            <figure>
-              <img src={props.image} alt="Book cover" />
-            </figure>
-          </div>
-          <div className="card-content">
-            <div className="media"></div>
-            <p className="content" style={{ textAlign: "right" }}>
-              {props.synopsis}
-            </p>
-          </div>
-          <div className="control">
-            <button className="button is-dark"
-          id= {""}
-          onClick={()=>props.handleDeleteButton("")}>
-            Save Book</button>
-            <a href={""}>
-            <button className="button is-dark">View Book</button>
-            </a>
+            <div className="card-image is-4by3">
+              <figure>
+                <img src={props.image} alt="Book cover" />
+              </figure>
+            </div>
+            <div className="card-content">
+              <div className="media"></div>
+              <p className="content" style={{ textAlign: "right" }}>
+                {props.synopsis}
+              </p>
+            </div>
+            <div className="control">
+              <a href={""}>
+                <button className="button is-dark">View Book</button>
+              </a>
+
+              <button
+                className="button is-dark"
+                id={""}
+                onClick={() => props.handleDeleteButton("")}
+              >
+                Save Book
+              </button>
+            </div>
           </div>
         </div>
       </div>
-      </div>
     </section>
-    )
+  );
 }
 
-export default SearchResult; 
+export default SearchResult;
